cca0b844045a87b96453642999aeb16c2e368d2d
Richard Tingstad <richard.tingstad@sparebank1.no>
2018-06-25 17:19:00 +0200

    assertTrue

---
+++ test/src/test/java/com/github/tingstad/junit4to5/asserts/assertTrue/StarImportTest.java
@@ -0,0 +1,12 @@
+package com.github.tingstad.junit4to5.asserts.assertTrue;
+
+import org.junit.*;
+
+public class StarImportTest {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+
+}
