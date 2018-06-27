3a7f9d4ec315c1ab8b50903df95c4dbe81267665
Richard Tingstad <richard.tingstad@sparebank1.no>
2018-06-27 14:10:35 +0200

    @Category ExcludeCategory

--- test/src/test/java/com/github/tingstad/junit4to5/category/CategoryTest.java
+++ test/src/test/java/com/github/tingstad/junit4to5/category/CategoryTest.java
@@ -5,28 +5,39 @@ import org.junit.experimental.categories.Category;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 @Category(FastTests.class)
 public class CategoryTest {
 
     @Test
-    public void test() throws Exception {
-        Annotation[] annotations = getClass().getAnnotations();
-        assertEquals(1, annotations.length);
-        Annotation annotation = getClass().getAnnotations()[0];
-        if (isUnit4()) {
-            assertEquals("org.jun" + "it.experimental.categories.Category", annotation.annotationType().getName());
+    @Category(SkipMe.class)
+    public void doNotRun() {
+        fail("Should not run");
+    }
+
+    @Test
+    @Category(FastTests.class)
+    public void testAnnotation() throws Exception {
+        Method method = getClass().getMethod("testAnnotation");
+        Annotation[] annotations = method.getAnnotations();
+        assertEquals(2, annotations.length);
+        Annotation annotation = Arrays.stream(annotations)
+            .filter(a -> !a.annotationType().getName().endsWith("Test"))
+            .findFirst().get();
+        if (isUnit4(method)) {
+            assertEquals("org.j" + "unit.experimental.categories.Category", annotation.annotationType().getName());
         } else {
             assertEquals("org.junit.jupiter.api.Tag", annotation.annotationType().getName());
         }
     }
 
-    private boolean isUnit4() throws NoSuchMethodException {
-        Method method = getClass().getMethod("test");
+    private boolean isUnit4(Method method) {
         Annotation annotation = method.getAnnotations()[0];
-        return annotation.annotationType().getName().equals("org.jun" + "it.Test");
+        return annotation.annotationType().getName().equals("org.j" + "unit.Test");
     }
 
 }
