20bdb49be7c708e738f0b5fbbd4544d429fb8426
Richard Tingstad <richard.tingstad@sparebank1.no>
2018-07-04 16:20:06 +0200

    assertTrue.StaticMethodImport

---
+++ test/src/test/java/com/github/tingstad/junit4to5/asserts/assertTrue/StaticMethodImport.java
@@ -0,0 +1,14 @@
+package com.github.tingstad.junit4to5.asserts.assertTrue;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class StaticMethodImport {
+
+    @Test
+    public void staticTest() {
+        assertTrue(true);
+    }
+
+}
