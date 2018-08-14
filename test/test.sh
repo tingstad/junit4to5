48711199d17e79e27b30676d0edfce52077c9fa7
Richard Tingstad <richard.tingstad@sparebank1.no>
2018-08-14 13:33:01 +0200

    Test script improvements
    Testdata in separate txt files instead of heredoc for better bash 3 compability.
    
    Increased sed compability.

--- test/test.sh
+++ test/test.sh
@@ -3,34 +3,17 @@ set -e
 
 DIR="$( cd "$(dirname "$0")" && pwd )"
 
-input="$(cat - <<- EOF
-	org.junit.Assert.assertTrue(true);
-	org.junit.Assert.assertTrue("msg", true);
-	org.junit.Assert.assertTrue("msg", foo().and(bar(3)));
-	org.junit.Assert.assertTrue("this" + "that )(", "hello, world".equals("hello, world"));
-	org.junit.Assert.assertTrue("\"hello, world\".equals(\"hello, world\")", "hello, world".equals("hello, world"));
-	org.junit.Assert.assertTrue("msg" + getSomething(), true);
-	org.junit.Assert.assertTrue("msg", method(
-	org.junit.Assert.assertTrue("esc\\"escesc\\\\\\"end", true);
-	org.junit.Assert.assertTrue("msg" /*****/, true);
-	EOF
-)"
-expected="$(cat - <<- EOF
-	org.junit.jupiter.api.Assertions.assertTrue(true);
-	org.junit.jupiter.api.Assertions.assertTrue( true,"msg");
-	org.junit.jupiter.api.Assertions.assertTrue( foo().and(bar(3)),"msg");
-	org.junit.jupiter.api.Assertions.assertTrue( "hello, world".equals("hello, world"),"this" + "that )(");
-	org.junit.jupiter.api.Assertions.assertTrue( "hello, world".equals("hello, world"),"\"hello, world\".equals(\"hello, world\")");
-	org.junit.jupiter.api.Assertions.assertTrue( true,"msg" + getSomething());
-	org.junit.jupiter.api.Assertions.assertTrue("msg", method(
-	org.junit.jupiter.api.Assertions.assertTrue( true,"esc\\"escesc\\\\\\"end");
-	org.junit.jupiter.api.Assertions.assertTrue( true,"msg" /*****/);
-	EOF
-)"
-diff <(echo "$expected") <(echo "$input" | sed --posix -E -f "$DIR/../junit4-to-5.sed")
+gnuver=$(sed --version 2>/dev/null | grep GNU | grep -E -o '[0-9]+\.[0-9]+' || true)
+if [ -n "$gnuver" ] && [ ${gnuver%.*} -lt 4 -o ${gnuver%.*} -eq 4 -a ${gnuver#*.} -lt 2 ]; then
+    args="-r"
+else
+    args="-E"
+fi
 
-#exit 0 #TODO remove
-mvn -f "$DIR"/junit4.pom.xml clean test
-find "$DIR"/src -name \*.java -exec sed -E -i.bak -f "$DIR/../junit4-to-5.sed" {} \;
-mvn -f "$DIR"/junit5.pom.xml clean test
+diff "$DIR/expected.txt" <(sed $args -f "$DIR/../junit4-to-5.sed" "$DIR/test.txt")
+
+mvn -f "$DIR/junit4.pom.xml" clean test
+find "$DIR/src" -name \*.java -exec sed $args -i.bak -f "$DIR/../junit4-to-5.sed" {} \;
+find "$DIR/src" -name \*.java.bak -delete
+mvn -f "$DIR/junit5.pom.xml" clean test
 
