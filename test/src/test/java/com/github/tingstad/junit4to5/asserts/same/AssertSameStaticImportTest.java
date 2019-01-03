7841d28752ce3df2207850698684b0de89aafc39
Richard Tingstad <richard.tingstad@gmail.com>
2019-01-03 10:20:57 +0100

    TCR

--- test/src/test/java/com/github/tingstad/junit4to5/asserts/same/AssertSameStaticImportTest.java
+++ test/src/test/java/com/github/tingstad/junit4to5/asserts/same/AssertSameStaticImportTest.java
@@ -12,6 +12,7 @@ public class AssertSameStaticImportTest {
         Object object = "hello";
         assertSame("Spaces", object, object);
 		assertSame("Tab indentation", object, object);
+        assertNotSame("not same", "one", "two");
     }
 
 }
