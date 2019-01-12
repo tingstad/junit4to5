17e7b70580d61c2788b024122b25d3613e70eb1f
Richard Tingstad <richard.tingstad@gmail.com>
2019-01-12 22:36:19 +0100

    TCR

--- test/src/test/java/com/github/tingstad/junit4to5/asserts/StarImportTest.java
+++ test/src/test/java/com/github/tingstad/junit4to5/asserts/StarImportTest.java
@@ -16,6 +16,7 @@ public class StarImportTest {
         Assert.assertNotNull("message", "hello");
         Assert.assertSame("same", Boolean.TRUE, Boolean.TRUE);
         Assert.assertNotSame("not same", Boolean.TRUE, Boolean.FALSE);
+        Assert.assertNotEquals("not equals", 1L, 2L);
     }
 
 }
